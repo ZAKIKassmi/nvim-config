9ff4b08b6c0c625977988d2353a5b84b65ebb9e2
Zakariae El Kassmi <elkassimi.zakariae012@gmail.com>
2025-12-31 15:43:52 +0100

    add noice.nvim to improve the overall UI

--- init.lua
+++ init.lua
@@ -1002,6 +1002,33 @@ require('lazy').setup({
     end,
   },
 
+  -- improve neovim UI, notification...
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'rcarriga/nvim-notify', -- Optional: for nice notifications
+    },
+    opts = {
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use Treesitter
+        override = {
+          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+          ['vim.lsp.util.stylize_markdown'] = true,
+          ['cmp.entry.get_documentation'] = true,
+        },
+      },
+      presets = {
+        bottom_search = true, -- use a classic bottom cmdline for search
+        command_palette = true, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = false, -- add a border to hover docs and signature help
+      },
+    },
+  },
+
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
